c5560ae2eed4f109ef77b50facf17bc41a241e27
César López-Natarén <cesarn@mindtouch.com>
2015-09-24 19:24:48 -0700

    wip

--- producer.go
+++ producer.go
@@ -7,6 +7,7 @@ import (
 	kinesis "github.com/sendgridlabs/go-kinesis"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -16,9 +17,11 @@ func main() {
 	var streamName string
 	flag.StringVar(&streamName, "streamName", "", "The name of the AWS Kinesis stream")
 	var sleepRange int64
-	flag.Int64Var(&sleepRange, "sleepRange", 1000, "The milliseconds to sleep between requests")
+	flag.Int64Var(&sleepRange, "sleepRange", 1, "The milliseconds to sleep between requests")
 	var producers uint
-	flag.UintVar(&producers, "producers", 2, "The number of concurrent producers")
+	flag.UintVar(&producers, "producers", 1, "The number of concurrent producers")
+	var eventCount int
+	flag.IntVar(&eventCount, "eventcount", 10, "The number of events to send")
 	flag.Parse()
 
 	// Validate the arguments
@@ -47,33 +50,36 @@ func main() {
 
 	// Loop forever
 	log.Printf("Will produce data to stream %v, using %v producers, sleeping %v ms in between", streamName, producers, sleepRange)
-	finished := make(chan bool)
+	var wg sync.WaitGroup
+
 	for p := uint(0); p < producers; p++ {
+		wg.Add(1)
 		go func(n uint) {
-			for {
+			defer wg.Done()
+			for i := 0; i < eventCount; i++ {
 				time.Sleep(time.Duration(sleepRange) * time.Millisecond)
 
 				// Batch insert records
 				args := kinesis.NewArgs()
 				args.Add("StreamName", streamName)
-				for i := 0; i < LIMIT; i++ {
-					args.AddRecord(
-						[]byte(EVENT),
-						fmt.Sprintf("partitionKey-%d", i))
-				}
+				args.AddRecord(
+					[]byte(fmt.Sprintf(EVENT, i)),
+					fmt.Sprintf("site_%d", i))
+
 				_, err := k.PutRecords(args)
+
 				if err != nil {
 					fmt.Printf("Error @ PutRecords, worker %v: %v\n\n", n, err)
 				} else {
-					fmt.Printf("Success PutRecords, worker %v, %v records\n", n, LIMIT)
+					fmt.Printf("Success PutRecords, worker %v, iteration %v\n", n, i)
 				}
 			}
 		}(p)
 	}
-	<-finished
+	wg.Wait()
 }
 
 const (
-	EVENT = `<event id="a3352360-8ec1-11e3-a063-8e4856e73110" datetime="2014-02-06T00:00:06Z" type="page:view" wikiid="site_1" journaled="false" version="2"> <request id="a3352360-8ec1-11e3-a063-0eb7d2788566" seq="1" count="1"><signature>POST:events/page-view/*</signature><ip>127.0.0.1</ip><session-id>005d41fa-f608-4bee-9db7-27da808100c0</session-id><parameters /><user id="2" anonymous="true" /></request><page id="1"><path>Page Path</path></page><data><_uri.host>host.io</_uri.host><_uri.scheme>https</_uri.scheme><_uri.query>refer=support</_uri.query></data></event>`
+	EVENT = `<event id="a3352360-8ec1-11e3-a063-8e4856e73110" datetime="2014-02-06T00:00:06Z" type="page:view" wikiid="site_1" journaled="false" version="2"> <request id="a3352360-8ec1-11e3-a063-0eb7d2788566" seq="1" count="1"><signature>POST:events/page-view/*</signature><ip>127.0.0.1</ip><session-id>005d41fa-f608-4bee-9db7-27da808100c0</session-id><parameters /><user id="2" anonymous="true" /></request><page id="1"><path>Page Path</path></page><data><_uri.host>host.io</_uri.host><_uri.scheme>https</_uri.scheme><_uri.query>refer=support</_uri.query><serial_number>%d</serial_number></data></event>`
 	LIMIT = 10
 )
